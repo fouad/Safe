ef2d6ae3a3776352f6ccd18946f7e4e325d745f6
Fouad Matin <fomatin@gmail.com>
2012-03-05 21:29:46 -0500

    comments

---
+++ inc/functions.php
@@ -0,0 +1,146 @@
+<?php
+
+function safe_get_serverinfo() {
+        global $wpdb;
+        $sqlversion = $wpdb->get_var("SELECT VERSION() AS version");
+        $mysqlinfo = $wpdb->get_results("SHOW VARIABLES LIKE 'sql_mode'");
+        if (is_array($mysqlinfo)) 
+            $sql_mode = $mysqlinfo[0]->Value;
+        if (empty($sql_mode)) 
+            $sql_mode = __('Not set');
+        $sm = ini_get('safe_mode');
+        if (strcasecmp('On', $sm) == 0) { 
+            $safe_mode = __('On'); 
+        }
+        else { 
+            $safe_mode = __('Off'); 
+        }
+        if(ini_get('allow_url_fopen')) 
+            $allow_url_fopen = __('On');
+        else 
+            $allow_url_fopen = __('Off');
+        if(ini_get('upload_max_filesize')) 
+            $upload_max = ini_get('upload_max_filesize');
+        else 
+            $upload_max = __('N/A');
+        if(ini_get('post_max_size')) 
+            $post_max = ini_get('post_max_size');
+        else 
+            $post_max = __('N/A');
+        if(ini_get('max_execution_time')) 
+            $max_execute = ini_get('max_execution_time');
+        else 
+            $max_execute = __('N/A');
+        if(ini_get('memory_limit')) 
+            $memory_limit = ini_get('memory_limit');
+        else 
+            $memory_limit = __('N/A');
+        if (function_exists('memory_get_usage')) 
+            $memory_usage = round(memory_get_usage() / 1024 / 1024, 2) . __(' MByte');
+        else 
+            $memory_usage = __('N/A');
+        if (is_callable('exif_read_data')) 
+            $exif = __('Yes'). " ( V" . substr(phpversion('exif'),0,4) . ")" ;
+        else 
+            $exif = __('No');
+        if (is_callable('iptcparse')) 
+            $iptc = __('Yes');
+        else 
+            $iptc = __('No');
+        if (is_callable('xml_parser_create')) 
+            $xml = __('Yes');
+        else 
+            $xml = __('No');
+
+?>
+        <li><?php _e('Operating System'); ?> : <strong><?php echo PHP_OS; ?></strong></li>
+        <li><?php _e('Server'); ?> : <strong><?php echo $_SERVER["SERVER_SOFTWARE"]; ?></strong></li>
+        <li><?php _e('Memory usage'); ?> : <strong><?php echo $memory_usage; ?></strong></li>
+        <li><?php _e('MYSQL Version'); ?> : <strong><?php echo $sqlversion; ?></strong></li>
+        <li><?php _e('SQL Mode'); ?> : <strong><?php echo $sql_mode; ?></strong></li>
+        <li><?php _e('PHP Version'); ?> : <strong><?php echo PHP_VERSION; ?></strong></li>
+        <li><?php _e('PHP Safe Mode'); ?> : <strong><?php echo $safe_mode; ?></strong></li>
+        <li><?php _e('PHP Allow URL fopen'); ?> : <strong><?php echo $allow_url_fopen; ?></strong></li>
+        <li><?php _e('PHP Memory Limit'); ?> : <strong><?php echo $memory_limit; ?></strong></li>
+        <li><?php _e('PHP Max Upload Size'); ?> : <strong><?php echo $upload_max; ?></strong></li>
+        <li><?php _e('PHP Max Post Size'); ?> : <strong><?php echo $post_max; ?></strong></li>
+        <li><?php _e('PHP Max Script Execute Time'); ?> : <strong><?php echo $max_execute; ?>s</strong></li>
+        <li><?php _e('PHP Exif support'); ?> : <strong><?php echo $exif; ?></strong></li>
+        <li><?php _e('PHP IPTC support'); ?> : <strong><?php echo $iptc; ?></strong></li>
+        <li><?php _e('PHP XML support'); ?> : <strong><?php echo $xml; ?></strong>  </li>
+<?php
+}
+
+function safe_check_table_prefix(){
+    if($GLOBALS['table_prefix']=='wp_'){
+        echo '<span class="fail">Your table prefix should not be <em>wp_</em>.</span><br />';
+    }
+    else { echo '<span class="pass">Your table prefix is not <i>wp_</i>.</span><br />'; }
+}
+
+function safe_errorsoff(){
+    echo '<span class="pass">WordPress DB Errors turned off.</span><br />';
+}
+
+function safe_wpdberrors()
+{
+    global $wpdb;
+    $wpdb->show_errors = false;
+}
+
+function safe_version_removal(){
+    global $wp_version;
+    echo '<span class="pass">Your WordPress version is successfully hidden.</span><br />';
+}
+
+function safe_check_version()
+{
+    $c = get_site_transient( 'update_core' );
+    if ( is_object($c))
+    {
+        if (empty($c->updates))
+        {
+            echo '<span class="pass">'.__('You have the latest version of Wordpress.').'</span>';
+            return;
+        }
+
+        if (!empty($c->updates[0]))
+        {
+            $c = $c->updates[0];
+
+            if ( !isset($c->response) || 'latest' == $c->response ) {
+                echo '<span class="pass">'.__('You have the latest version of Wordpress.').'</span>';
+                return;
+            }
+
+            if ('upgrade' == $c->response)
+            {
+                $lv = $c->current;
+                $m = '<span class="fail">'.sprintf('Wordpress <strong>(%s)</strong> is available. You should upgrade to the latest version.', $lv).'</span>';
+                echo __($m);
+                return;
+            }
+        }
+    }
+
+    echo '<span class="fail">'.__('An error has occurred while trying to retrieve the status of your Wordpress version.').'</span>';
+}
+function safe_wpConfigCheckPermissions($wpConfigFilePath)
+{
+    if (!is_writable($wpConfigFilePath)) { 
+        echo '<span class="pass">'.__('Your wp config file is not a threat.').'</span>';
+        return false; 
+    }
+
+    if (!function_exists('file') || !function_exists('file_get_contents') || !function_exists('file_put_contents'))
+    {
+        echo '<span class="pass">'.__('Your wp config file is not a threat.').'</span>';
+        return false;
+    }
+    else {
+        echo '<span class="fail">'.__('Your wp config file can be compromised by hackers, fix the permissions.').'</span>';
+        return true;
+    }
+}
+
+?>
--- safe.php
+++ safe.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Safe
-Plugin URI: http://www.tutsplus.com
+Plugin URI: http://wp.tutsplus.com
 
-Description: Your very own butler
+Description: The easiest, most effective way to secure your WordPress site from attackers.
 Author: Fouad Matin
 Version: 1.0
 Author URI: http://wp.tutsplus.com/author/fouad
@@ -21,32 +21,18 @@ if ( ! defined('WP_PLUGIN_DIR')) {
     define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
 }
 
-require_once(WP_PLUGIN_DIR . "/alfred/libs/functions.php");
-// require_once(WP_PLUGIN_DIR . "/safe/libs/safe.php");
+require_once(WP_PLUGIN_DIR . "/safe/inc/functions.php");
 
-
-//## this is the container for header scripts
-add_action('admin_head', 'mrt_hd');
-// # $rev #2 {c}
+add_action('admin_head', 'safe_hd');
 add_action('admin_init', 'wps_admin_init_load_resources');
-
-//before sending headers
-add_action("init",'mrt_wpdberrors',1);
-
-//after executing a query
-add_action("parse_query",'mrt_wpdberrors',1);
-
-//## add the sidebar menu
+add_action("init",'safe_wpdberrors',1);
+add_action("parse_query",'safe_wpdberrors',1);
 add_action('admin_menu', 'add_men_bpg');
-
-// add_action("init", 'mrt_remove_wp_version',1);   //comment out this line to make ddsitemapgen work
-
-//before rendering each admin init
 add_action('admin_init','safe_admin_init');
 add_filter('admin_footer_text', 'remove_footer_admin');
 
 function remove_footer_admin () {
-    echo "Thank you for creating with <a href='http://wordpress.org'>WordPress</a>. Also, thank you for using the safe security plugin by <a href='http://fouadmat.in'>Fouad Matin</a>.";
+    echo "Thank you for creating with <a href='http://wordpress.org'>WordPress</a>. Also, thank you for using the Safe security plugin by <a href='http://fouadmat.in'>Fouad Matin</a>.";
 } 
 
 function safe_admin_init(){
@@ -60,86 +46,70 @@ function add_men_bpg()
 
     if (function_exists('add_menu_page'))
     {
-        add_menu_page('Safe', 'Safe', 'edit_pages', __FILE__, 'mrt_opt_mng_pg', WP_PLUGIN_URL.'/safe/img/safe.png');
+        add_menu_page('Safe', 'Safe', 'edit_pages', __FILE__, 'safe_main', WP_PLUGIN_URL.'/safe/img/safe.png');
     }
 }
-// add_filter( 'plugin_action_links_'.plugin_basename(__FILE__), 'wpss_admin_plugin_actions', -10);
 
 function safe_meta_box()
 {
 ?>
-    <div id="safe-initial-scan" class="safe-inside">
-        <div class="safe-initial-scan-section"><?php mrt_check_version();?></div>
+    <div id="safe-basic-checks" class="safe-inside">
+        <div class="safe-basic-checks-section"><?php safe_check_version();?></div>
 
-        <div class="safe-initial-scan-section"><?php mrt_check_table_prefix();?></div>
+        <div class="safe-basic-checks-section"><?php safe_check_table_prefix();?></div>
 
-        <div class="safe-initial-scan-section"><?php mrt_version_removal();?></div>
+        <div class="safe-basic-checks-section"><?php safe_version_removal();?></div>
 
-        <div class="safe-initial-scan-section"><?php mrt_errorsoff();?></div>
+        <div class="safe-basic-checks-section"><?php safe_errorsoff();?></div>
+
+        <div class="safe-basic-checks-section"><?php safe_wpConfigCheckPermissions('/wp-config.php');?></div>
 <?php
     global $wpdb;
 
-    echo '<div class="scanpass">WP ID META tag removed form WordPress core</div>';
+    echo '<div class="pass">WP ID META tag removed form WordPress core</div>';
 
-    echo '<div class="safe-initial-scan-section">';
+    echo '<div class="safe-basic-checks-section">';
         $name = $wpdb->get_var("SELECT user_login FROM $wpdb->users WHERE user_login='admin'");
         if ($name == "admin") {
-                echo '<font color="red">"admin" user exists.</font>';
+                echo '<span class="fail">"admin" user exists.</span>';
         }
-        else { echo '<span class="scanpass">No user "admin".</span>'; }
+        else { echo '<span class="pass">No user "admin".</span>'; }
     echo '</div>';
 
-    echo '<div class="safe-initial-scan-section">';
+    echo '<div class="safe-basic-checks-section">';
         if (file_exists('.htaccess')) {
-            echo '<span class="scanpass">.htaccess file found in wp-admin/</span>';
+            echo '<span class="pass">.htaccess file found in wp-admin/</span>';
         }
-        else { echo '<span style="color:#f00;">
-            The file .htaccess does not exist in the wp-admin section.
-            Read more why you should have a .htaccess file in  the WP-admin area
-            <a href="http://www.websitedefender.com/wordpress-security/htaccess-files-wordpress-security/"
-            title="Why you should have a .htaccess file in  the WP-admin area" target="_blank">here</a>.
-            </span>'; }
+        else { echo '<span class="fail">The file .htaccess does not exist in the wp-admin section.</span>'; }
     echo '</div>';
 
 ?>
-
-        <div class="mrt_wpss_note">
-            <em>**WP Security Scan plugin <strong>must</strong> remain active for security features to persist**</em>
-        </div>
     </div>
-    sadf
 <?php
 }
 function safe_meta_box2()
 {
 ?>
     <ul id="safe-information-scan-list">
-            <?php mrt_get_serverinfo(); ?>
+            <?php safe_get_serverinfo(); ?>
     </ul>
 <?php
 }
-function safe_render_main()
-{
-  echo 'tits';
-}
-function mrt_opt_mng_pg() {
+function safe_main() {
 
-            add_meta_box("wpss_mrt_1", 'Initial Scan', "safe_meta_box", "wpss");
-
-            add_meta_box("wpss_mrt_3", 'About Website Defender', "safe_render_main", "wpss_safe");
-            add_meta_box("wpss_mrt_2", 'System Information Scan', "safe_meta_box2", "wpss2");
+            add_meta_box("safe_box_1", 'Basic Checks', "safe_meta_box", "box1");
+            add_meta_box("safe_box_2", 'System Information', "safe_meta_box2", "box2");
 
 echo '  
             <div class="metabox-holder">
                 <div style="float:left; width:48%;" class="inner-sidebar1">';
          
-                    do_meta_boxes('wpss','advanced','');    
-                    do_meta_boxes('wpss_safe','advanced','');   
+                    do_meta_boxes('box1','advanced','');      
 
 echo '      
                 </div>
                 <div style="float:right;width:48%;" class="inner-sidebar1">';
-                    do_meta_boxes('wpss2','advanced','');  
+                    do_meta_boxes('box2','advanced','');  
 echo '  
                 </div>
                         
